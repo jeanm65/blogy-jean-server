9c0c90f42c3e2e462a25306c234173d96de96499
Jean Marie <jm.randrianant65@gmail.com>
2026-05-22 07:53:09 +0800

    convert import to ES style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const cors = require("cors")
+import express, { urlencoded, json, static } from "express";
+import cors from "cors";
 
 const PORT = 8082;
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(express.static('public'))
+app.use(urlencoded({ extended: false }))
+app.use(json())
+app.use(static('public'))
 
 const articles = [
   {
